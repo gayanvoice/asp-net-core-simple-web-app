ea572aac28348df1033132e223e12474fc4c1782
Gayan Kuruppu <gayan.kuruppu@live.com>
2021-09-17 11:57:23 +0530

    Add New Field

--- WebApp/Migrations/20210917061914_Rating.Designer.cs
+++ WebApp/Migrations/20210917061914_Rating.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using WebApp.Data;
 
 namespace WebApp.Migrations
 {
     [DbContext(typeof(MovieContext))]
-    partial class MovieContextModelSnapshot : ModelSnapshot
+    [Migration("20210917061914_Rating")]
+    partial class Rating
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- WebApp/Models/Movie.cs
+++ WebApp/Models/Movie.cs
@@ -15,5 +15,6 @@ namespace WebApp.Models
         public string Genre { get; set; }
         [Column(TypeName = "decimal(18, 2)")]
         public decimal Price { get; set; }
+        public string Rating { get; set; }
     }
 }
