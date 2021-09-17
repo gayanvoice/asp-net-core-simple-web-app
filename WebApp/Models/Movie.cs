d0035492317f44098363f016d7edbc36d4a2a8ec
Gayan Kuruppu <gayan.kuruppu@live.com>
2021-09-17 10:29:44 +0530

    Refactor Movie.cs

--- WebApp/Models/Movie.cs
+++ WebApp/Models/Movie.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace WebApp.Models
 {
@@ -8,9 +9,13 @@ namespace WebApp.Models
         public int Id { get; set; }
         public string Title { get; set; }
 
+        [Display(Name = "Release Date")]
         [DataType(DataType.Date)]
         public DateTime ReleaseDate { get; set; }
+
+        [Display(Name = "Movie Genre")]
         public string Genre { get; set; }
+        [Column(TypeName = "decimal(18, 2)")]
         public decimal Price { get; set; }
     }
 }
